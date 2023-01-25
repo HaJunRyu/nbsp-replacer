ea233f5649b317219dd5ef9dc4acefd467fefe55
HaJunRyu <hjr4557@naver.com>
2023-01-25 22:37:21 +0900

    Feat: nbsp변환 기능들에 쓰이는 요소들에 style을 적용한다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,89 @@
+import styled from '@emotion/styled';
 import Head from 'next/head';
 import { ChangeEventHandler, useState } from 'react';
+
+const Main = styled.main`
+  padding: 8rem 2.4rem;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 1.6rem;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  max-width: 76.8rem;
+  height: 30rem;
+  border: 0.1rem solid #dee2e6;
+  border-radius: 12px;
+  background: #ffffff;
+  padding: 1.2rem 1.4rem;
+  outline: 0;
+  resize: none;
+  color: #495057;
+
+  &::selection {
+    background: #c2b6ff;
+  }
+
+  @media (hover: hover) {
+    &:hover {
+      border: 0.1rem solid #c2b6ff;
+    }
+  }
+
+  &:active {
+    border: 0.1rem solid #c2b6ff;
+  }
+
+  &:focus {
+    border: 0.1rem solid #6244ff;
+  }
+
+  &:disabled {
+    color: #868e96;
+    background: #f8f9fa;
+    border: 0.1rem solid #dee2e6;
+    opacity: 1;
+    -webkit-text-fill-color: #868e96;
+  }
+
+  &::placeholder {
+    color: #adb5bd;
+  }
+`;
+
+const ReplaceButton = styled.button`
+  margin-top: 1.6rem;
+  height: 5.6rem;
+  padding: 1.6rem 1.95rem;
+  color: #ffffff;
+  background: #6244ff;
+  border: 0;
+  border-radius: 1.6rem;
+  transition: 0.3s;
+  font-weight: 500;
+  font-size: 1.6rem;
+  line-height: 1.5;
+  letter-spacing: -0.08rem;
+
+  @media (hover: hover) {
+    &:hover {
+      background: #4127d1;
+    }
+  }
+
+  &:active {
+    background: #4127d1;
+  }
+
+  &:disabled {
+    background: #c2b6ff;
+  }
+`;
 
 export default function Home() {
   const [textAreaValue, setTextAreaValue] = useState('');
@@ -15,6 +99,8 @@ export default function Home() {
 
     setTextAreaValue(replacedNbsp);
   };
+
+  const isDisabled = textAreaValue === '';
   return (
     <>
       <Head>
@@ -23,12 +109,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <textarea value={textAreaValue} onChange={handleChangeTextAreaValue} />
-        <button type="button" onClick={handleReplaceNbsp}>
+      <Main>
+        <Title>nbsp 변환기</Title>
+        <TextArea value={textAreaValue} onChange={handleChangeTextAreaValue} />
+        <ReplaceButton type="button" onClick={handleReplaceNbsp} disabled={isDisabled}>
           변환하기
-        </button>
-      </main>
+        </ReplaceButton>
+      </Main>
     </>
   );
 }
